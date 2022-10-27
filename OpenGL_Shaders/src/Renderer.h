d3e0e5bcbd563711a7f6c73488d88313318dde4e
Kallegan <90606627+Kallegan@users.noreply.github.com>
2022-10-27 23:02:24 +0200

    Improved renderer.

--- OpenGL_Shaders/src/Renderer.h
+++ OpenGL_Shaders/src/Renderer.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <GL/glew.h>
-#include <iostream>
+
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
 
 #define ASSERT(x) if (!(x)) __debugbreak();
 #define GLCall(x) GLClearError();\
@@ -11,3 +14,11 @@
 
 void GLClearError(); //clear all the errors.
 bool GLLogCall(const char* function, const char* file, int line);
+
+
+class Renderer
+{
+public:
+    void Clear() const;
+    void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+};
--- OpenGL_Shaders/src/Shader.cpp
+++ OpenGL_Shaders/src/Shader.cpp
@@ -111,6 +111,12 @@ void Shader::Unbind() const
     GLCall(glUseProgram(0));
 }
 
+void Shader::SetUniform1f(const std::string& name, float value)
+{
+    GLCall(glUniform1f(GetUnitformLocation(name), value));
+}
+
+
 void Shader::SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3)
 {
     GLCall(glUniform4f(GetUnitformLocation(name), v0, v1, v2, v3));
