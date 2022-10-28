24f4f2966d233cf0f7dd0f42317b21a4e1fb4c53
Kallegan <90606627+Kallegan@users.noreply.github.com>
2022-10-28 02:08:14 +0200

    maths header lib

--- OpenGL_Shaders/src/Shader.cpp
+++ OpenGL_Shaders/src/Shader.cpp
@@ -127,6 +127,11 @@ void Shader::SetUniform4f(const std::string& name, float v0, float v1, float v2,
     GLCall(glUniform4f(GetUnitformLocation(name), v0, v1, v2, v3));
 }
 
+void Shader::SetUniformMat4f(const std::string& name, const glm::mat4& matrix)
+{
+    GLCall(glUniformMatrix4fv(GetUnitformLocation(name), 1, GL_FALSE, &matrix[0][0]));
+}
+
 int Shader::GetUnitformLocation(const std::string& name)
 {
     if (m_UniformLocationCache.find(name) != m_UniformLocationCache.end())
